791652543d89eb9f72aa1db8165a93b9e435f47e
Sébastien DAVOULT <d@vou.lt>
2020-09-15 22:24:54 +0200

    Initial Commit

---
+++ src/test/java/fr/axians/qiot/edge_service/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package fr.axians.qiot.edge_service;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
