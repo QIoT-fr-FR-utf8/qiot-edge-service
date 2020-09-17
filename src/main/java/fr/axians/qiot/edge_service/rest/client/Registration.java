2cfebf5e5684491a8a0db58d25833d9333bd9d6a
chamseddine.saadoune <chamseddine.saadoune@axians.com>
2020-09-18 00:44:01 +0200

    Adding usefull imports

--- src/main/java/fr/axians/qiot/edge_service/rest/client/Registration.java
+++ src/main/java/fr/axians/qiot/edge_service/rest/client/Registration.java
@@ -1,15 +1,19 @@
 package fr.axians.qiot.edge_service;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path("/register")
+@ApplicationScoped
 @Produces(MediaType.TEXT_PLAIN)
 @Consumes(MediaType.TEXT_PLAIN)
 public class Registration {
