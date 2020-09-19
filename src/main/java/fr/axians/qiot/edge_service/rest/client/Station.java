66b5deb803c503a5085f572f029ff6272304c6da
chamseddine.saadoune <chamseddine.saadoune@axians.com>
2020-09-19 14:24:31 +0200

    Modify Station class

--- src/main/java/fr/axians/qiot/edge_service/rest/client/Station.java
+++ src/main/java/fr/axians/qiot/edge_service/rest/client/Station.java
@@ -3,9 +3,9 @@ package fr.axians.qiot.edge_service.rest.client;
 public class Station {
 
     public Integer id;
-    public String serial = "36c2a3079e97428fa514f2beb0fd4e6a";
-    public String name = "FRutf8";
-    public double longitude = 2.3;
-    public double latitude = 48.8;
+    public String serial;
+    public String name;
+    public double longitude;
+    public double latitude;
 
 }
