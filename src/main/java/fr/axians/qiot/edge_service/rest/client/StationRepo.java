d0a51ba31a3af30d3299270daad41ecb14ae2302
chamseddine.saadoune <chamseddine.saadoune@axians.com>
2020-09-19 16:39:34 +0200

    Test

--- src/main/java/fr/axians/qiot/edge_service/rest/client/StationRepo.java
+++ src/main/java/fr/axians/qiot/edge_service/rest/client/StationRepo.java
@@ -4,9 +4,7 @@ import fr.axians.qiot.edge_service.rest.client.Station;
 
 public class StationRepo {
     public Station findStationbyId(int stationId){
-        Station st;
-        if (st.getId() == null)
-            return null;
+        Station st = new Station();
         return st;
     }
 }
