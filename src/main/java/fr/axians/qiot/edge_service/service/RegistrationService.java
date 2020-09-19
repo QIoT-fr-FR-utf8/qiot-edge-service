ebbc2d2b020983724c37fe8e2f38cc8acc5d9438
chamseddine.saadoune <chamseddine.saadoune@axians.com>
2020-09-19 10:33:57 +0200

    Minor modification

--- src/main/java/fr/axians/qiot/edge_service/service/RegistrationService.java
+++ src/main/java/fr/axians/qiot/edge_service/service/RegistrationService.java
@@ -1,5 +1,6 @@
 package fr.axians.qiot.edge_service.service;
 
+@ApplicationScoped
 public interface RegistrationService {
 
     int register(String serial, String name, double longitude, double latitude);
