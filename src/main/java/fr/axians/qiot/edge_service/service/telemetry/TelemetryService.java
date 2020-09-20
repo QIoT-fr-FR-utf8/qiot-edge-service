cba582b40677c6ca9a8da2d3655cf3230dffc34c
chamseddine.saadoune <chamseddine.saadoune@axians.com>
2020-09-20 12:44:56 +0200

    AMQ implementation and adding gas object for a test

--- src/main/java/fr/axians/qiot/edge_service/service/telemetry/TelemetryService.java
+++ src/main/java/fr/axians/qiot/edge_service/service/telemetry/TelemetryService.java
@@ -1,5 +1,13 @@
 package fr.axians.qiot.edge_service.service.telemetry;
 
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+
 public class TelemetryService {
 
+    /* Pollution stream */
+    @Outgoing("gas-stream")
+    public String streamData(String data) {
+        return data;
+        }
 }
