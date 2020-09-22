4d9f5021a3c76234eb68ff45726345b0086e7e09
Sébastien DAVOULT <d@vou.lt>
2020-09-22 18:45:56 +0200

    Defini Pollution class

--- src/main/java/fr/axians/qiot/edge_service/service/sensor/PollutionResult.java
+++ src/main/java/fr/axians/qiot/edge_service/service/sensor/PollutionResult.java
@@ -1,9 +1,18 @@
 package fr.axians.qiot.edge_service.service.sensor;
 
-
 public class PollutionResult{
-    public Double nh3;
-    public Double oxidising;
-    public Double reducing;
+    public Integer PM10;
+    public Integer PM10_atm;
+    public Integer PM1_0;
+    public Integer PM1_0_atm;
+    public Integer PM2_5;
+    public Integer PM2_5_atm;
+    public Integer gt0_3um;
+    public Integer gt0_5_um;
+    public Integer gt10um;
+    public Integer gt1_0um;
+    public Integer gt2_5um;
+    public Integer gt5_0um;
+    public String instant;
 
 }
